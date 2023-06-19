ab7cd37b83d3aab8fa5c90285b785f18b8cd265a
Sergi Philipsen <philipsen.sergi@gmail.com>
2023-06-19 13:55:20 +0200

    feat: change charts folder, add frontend and fix icons

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxthq/ui',
+  ],
+  content: {
+    sources: {
+      charts: {
+        driver: 'github',
+        prefix: '/charts',
+        repo: 'ibissource/charts',
+        branch: 'master',
+        dir: 'charts'
+      },
+      index: {
+        driver: 'github',
+        prefix: '/charts',
+        repo: 'ibissource/charts',
+        branch: 'gh-pages',
+        dir: ''
+      },
+    },
+    ignores: [
+      '/templates/'
+    ],
+    highlight: {
+      theme: 'github-light'
+    }
+  },
+  colorMode: {
+    preference: 'light'
+  }
+})
+
